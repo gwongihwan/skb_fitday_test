69a0f86081f5a9d5188ab177019460eb9cad4194
P145337 <realuv@partner.sk.com>
2019-06-14 17:49:15 +0900

    .

--- src/main/java/com/skb/fitday/springboot/api/TestController.java
+++ src/main/java/com/skb/fitday/springboot/api/TestController.java
@@ -1,9 +1,8 @@
 package com.skb.fitday.springboot.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    private static final Logger logger = LoggerFactory.getLogger("Application");
+//    private static final Logger logger = LoggerFactory.getLogger("Application");
 
-    @PostMapping("/{param1}")
-    public ResponseEntity<String> home(String param) {
-        logger.debug(this.getClass().getName() + " > " + "아몰랑 메소드명 찍고" + " > " + param);
+    @PostMapping("/home/{param}")
+    public ResponseEntity<String> home(@PathVariable String param) {
+//        logger.debug(this.getClass().getName() + " > " + "아몰랑 메소드명 찍고" + " > " + param);
+        System.out.println(param);
         return new ResponseEntity<String>("okokokok", HttpStatus.OK);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 
 }
